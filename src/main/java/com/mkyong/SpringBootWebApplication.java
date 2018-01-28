51b4bb158dd79f32a0f5b81e5e4cfa88a072984b
lijiau <li.ji@mydreamplus.com>
2018-01-28 12:02:14 +0800

    hello

--- src/main/java/com/mkyong/SpringBootWebApplication.java
+++ src/main/java/com/mkyong/SpringBootWebApplication.java
@@ -1,6 +1,5 @@
 package com.mkyong;
 
-import com.mapper.UserMapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,13 +14,7 @@ public class SpringBootWebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
         ConfigurableApplicationContext context = SpringApplication.run(SpringBootWebApplication.class, args);
-        UserMapper userMapper = context.getBean(UserMapper.class);
-
-        System.out.println(userMapper.findAll(0, 10).size());
-        userMapper.findAll(1, 10).forEach(user -> {
-            System.out.println(user);
-        });
-        System.out.println("count user:" + userMapper.countUser());
+        System.out.println("let us hack facebook!");
     }
 
 }
